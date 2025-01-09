1555c3c47a3ff5e8d34abbf41228761d41f787d5
Allan Mehlen <mehlen.allan@gmail.com>
2025-01-09 06:44:44 +0100

    feat: set application icon

--- src/main/java/de/amehlen/App.java
+++ src/main/java/de/amehlen/App.java
@@ -2,7 +2,10 @@ package de.amehlen;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
 
@@ -10,6 +13,10 @@ public class App extends Application {
     public void start(Stage stage) {
         ChessBoard chessBoard = new ChessBoard();
         Scene scene = new Scene(chessBoard, 640, 640);
+
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/chess-icon.png")));
+        stage.getIcons().add(icon);
+
         stage.setTitle("Chess Game");
         stage.setScene(scene);
         stage.show();
