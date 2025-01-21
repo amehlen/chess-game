1f652754e7ab2e688909b8552eca99cc5eb7e24c
Allan Mehlen <mehlen.allan@gmail.com>
2025-01-21 13:23:45 +0100

    feat(gui): add menu item to open chess files

--- src/main/java/de/amehlen/gui/MenuHandler.java
+++ src/main/java/de/amehlen/gui/MenuHandler.java
@@ -3,9 +3,16 @@ package de.amehlen.gui;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 public class MenuHandler {
+
+    private static final Logger LOGGER = LogManager.getLogger(MenuHandler.class);
 
     private final Stage stage;
 
@@ -18,16 +25,29 @@ public class MenuHandler {
 
         Menu fileMenu = new Menu("File");
 
+        MenuItem openMenuItem = new MenuItem("Open");
+        openMenuItem.setOnAction(e -> openFile());
+
         MenuItem closeMenuItem = new MenuItem("Close");
         closeMenuItem.setOnAction(e -> closeApplication());
 
         fileMenu.getItems()
-                .addAll(closeMenuItem);
+                .addAll(openMenuItem, closeMenuItem);
 
         menuBar.getMenus()
                .add(fileMenu);
 
         return menuBar;
+    }
+
+    private void openFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters()
+                   .add(new FileChooser.ExtensionFilter("Chess Files", "*.chess"));
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            LOGGER.info("File selected: {}", file.getAbsolutePath());
+        }
     }
 
     private void closeApplication() {
